5d1608e0ebddccc3f09d6fafbf1fd7a2d767f97d
TOMKIBOI <tomkiboi@yahoo.com>
2023-10-11 13:43:31 -0700

    fix error

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
-import sys
-from calculator_1 import add, sub, mul, div
+from sys import argv
+from calculator_1 import add, div, mul, sub
 
-if len(sys.argv) != 4:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    sys.exit(1)
 
-a = int(sys.argv[1])
-operator = sys.argv[2]
-b = int(sys.argv[3])
+def main():
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    a = int(argv[1])
+    b = int(argv[3])
+    if argv[2] == '+':
+        print("{} {} {} = {}".format(a, argv[2], b, add(a, b)))
+    elif argv[2] == '-':
+        print("{} {} {} = {}".format(a, argv[2], b, sub(a, b)))
+    elif argv[2] == '*':
+        print("{} {} {} = {}".format(a, argv[2], b, mul(a, b)))
+    elif argv[2] == '/':
+        print("{} {} {} = {}".format(a, argv[2], b, div(a, b)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
 
-if operator == '+':
-    result = add(a, b)
-elif operator == '-':
-    result = sub(a, b)
-elif operator == '*':
-    result = mul(a, b)
-elif operator == '/':
-    result = div(a, b)
-else:
-    print("Unknown operator. Available operators: +, -, * and /")
-    sys.exit(1)
-
-print("{} {} {} = {}".format(a, operator, b, result))
+if __name__ == "__main__":
+    main()
--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,3 +1,2 @@
 #!/usr/bin/python3
-import builtins
-builtins.print("#pythoniscool")
+f = open(1, 'w'); f.write("#pythoniscool\n"); f.close()
