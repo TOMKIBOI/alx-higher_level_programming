69ab4cb316adf9098e568c0e3f3195dd8f74f04d
TOMKIBOI <tomkiboi@yahoo.com>
2023-10-13 15:27:11 -0700

    error

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,6 @@
+#!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    sorted_keys = sorted(a_dictionary.keys())
-    
-    for key in sorted_keys:
-        print("{}: {}".format(key, a_dictionary[key]))
+    list_d = list(a_dictionary.keys())
+    list_d.sort()
+    for i in list_d:
+        print("{}: {}".format(i, a_dictionary.get(i)))
