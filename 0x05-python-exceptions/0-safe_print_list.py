969a82c40c83df0796658bbe6b355d891f0a361b
TOMKIBOI <tomkiboi@yahoo.com>
2023-10-25 15:29:44 -0700

    pycodestyle

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,11 +5,10 @@ def safe_print_list(my_list=[], x=0):
     for y in range(x):
         try:
             print("{}".format(my_list[y]), end='')
-        except:
+        except Exception:
             break
         else:
             count += 1
 
     print()
     return (count)
-
