303a9695d3d37457bee08b69d7f556cb9db57663
TOMKIBOI <tomkiboi@yahoo.com>
2023-12-15 15:52:50 -0800

    fixed error

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,3 @@
--- Script that creates a user
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost'
-IDENTIFIED BY 'user_0d_1_pwd';
+-- Srcipt that creates the user user_0d_1 and grant all priviledges
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO user_0d_1@loca
