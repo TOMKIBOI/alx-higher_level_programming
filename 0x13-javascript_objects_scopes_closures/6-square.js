3b046a440dd541be127ef999608ae93ead285a30
TOMKIBOI <tomkiboi@yahoo.com>
2024-01-10 18:17:54 -0800

    exec files

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const SquarePrin = require('./5-square');
+
+class Square extends SquarePrin {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += c;
+      }
+      console.log(s);
+    }
+  }
+}
+
+module.exports = Square;
+
