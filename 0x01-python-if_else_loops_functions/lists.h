fdc29feeaa2c6a74922ac448515443bbd3bcfe38
TOMKIBOI <tomkiboi@yahoo.com>
2024-01-10 17:31:09 -0800

    initial files

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -3,6 +3,19 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * struct listint_s -linked list
+ * @n: int
+ * @next: the next node
+ *
+ * Description: linked list node
+ *
+ */
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *next;
+} listint_t;
 
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
