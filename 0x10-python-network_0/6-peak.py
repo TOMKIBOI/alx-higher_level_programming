247871724c5fecdff9a23c853d04cdf9f848e107
TOMKIBOI <tomkiboi@yahoo.com>
2024-02-01 07:56:42 -0800

    time complexity

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,25 +1,28 @@
 #!/usr/bin/python3
-""" Finds a peak within a list """
+""" function that finds a peak in a list of unsorted integers """
 
 
 def find_peak(list_of_integers):
-    if list_of_integers == []:
+    if not list_of_integers:
         return None
 
-    length = len(list_of_integers)
-    mid = int(length / 2)
-    li = list_of_integers
+    low, high = 0, len(list_of_integers) - 1
 
-    if mid - 1 < 0 and mid + 1 >= length:
-        return li[mid]
-    elif mid - 1 < 0:
-        return li[mid] if li[mid] > li[mid + 1] else li[mid + 1]
-    elif mid + 1 >= length:
-        return li[mid] if li[mid] > li[mid - 1] else li[mid - 1]
+    while low < high:
+        mid = (low + high) // 2
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            high = mid
+        else:
+            low = mid + 1
 
-    if li[mid - 1] < li[mid] > li[mid + 1]:
-        return li[mid]
+    return list_of_integers[low]
 
-    if li[mid + 1] > li[mid - 1]:
-        return find_peak(li[mid:])
-    return find_peak(li[:mid])
+
+# Example usage
+if __name__ == "__main__":
+    print(find_peak([1, 2, 4, 6, 3]))
+    print(find_peak([4, 2, 1, 2, 3, 1]))
+    print(find_peak([2, 2, 2]))
+    print(find_peak([]))
+    print(find_peak([-2, -4, 2, 1]))
+    print(find_peak([4, 2, 1, 2, 2, 2, 3, 1]))
